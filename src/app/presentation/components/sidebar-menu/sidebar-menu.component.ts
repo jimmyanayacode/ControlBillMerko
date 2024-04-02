cf122d67807ca55b73608aed3fb040c33a8d2ed3
JimmyCode <jimmyanayacode@gmail.com>
2024-04-02 07:49:46 -0500

    branch refactor sidebar

--- src/app/presentation/components/sidebar-menu/sidebar-menu.component.ts
+++ src/app/presentation/components/sidebar-menu/sidebar-menu.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'sidebar-menu',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule, MatSidenavModule, MatButtonModule],
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.css'
 })
