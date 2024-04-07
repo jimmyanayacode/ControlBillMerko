756ef3c3dda35bb91278629c77f8c9bb706b5f5e
JimmyCode <jimmyanayacode@gmail.com>
2024-04-06 19:08:27 -0500

    created filter control component

---
+++ src/app/presentation/components/filterControl/filter-control/filter-control.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-control.component.html',
+  styleUrl: './filter-control.component.css'
+})
+export class FilterControlComponent {
+
+}
