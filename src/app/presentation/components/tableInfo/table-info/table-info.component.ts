bc73f3d3c49fbeadef8c00b906a24f58a2436786
JimmyCode <jimmyanayacode@gmail.com>
2024-04-21 19:08:46 -0500

    PreRefactor interface

--- src/app/presentation/components/tableInfo/table-info/table-info.component.ts
+++ src/app/presentation/components/tableInfo/table-info/table-info.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+/* Angular Material */
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import { ProvidersWithInvoiceDetails } from '../../../../interfaces/provider/providerDetailsInvoice.interface';
-import { BillFromBackend } from '../../../../interfaces/bill/billFromBackend,interface';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 
 @Component({
   selector: 'component-table',
   standalone: true,
-  imports: [ MatFormFieldModule, MatInputModule, MatTableModule, CommonModule ],
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, CommonModule],
   templateUrl: './table-info.component.html',
   styleUrl: './table-info.component.css'
 })
@@ -19,27 +19,20 @@ export class TableInfoComponent implements OnChanges {
 
   @Input() filterProperty: boolean = false
   @Input() dataTable: any[] = []
-  @Input() displayedColumnsDinamic:{ title: string, key: string }[] = []
-  /* @Input() billsData: BillFromBackend[] = [] */
+  @Input() displayedColumnsDinamic: { title: string, key: string }[] = []
 
   displayedColumns: string[] = [];
   dataSource = new MatTableDataSource<any>(this.dataTable);
 
-constructor(){}
-
   ngOnChanges(changes: SimpleChanges): void {
-    if ( changes['displayedColumnsDinamic']) this.displayedColumns = changes['displayedColumnsDinamic'].currentValue.map((col:any) => col.key );
-    if (changes['dataTable'] ) this.dataSource.data = changes['dataTable'].currentValue;
+    if (changes['displayedColumnsDinamic']) this.displayedColumns = changes['displayedColumnsDinamic'].currentValue.map((col: any) => col.key);
+    if (changes['dataTable']) this.dataSource.data = changes['dataTable'].currentValue;
   }
 
   applyFilter(event: Event) {
-   const filterValue = (event.target as HTMLInputElement).value;
-   console.log(filterValue)
+    const filterValue = (event.target as HTMLInputElement).value;
+    console.log(filterValue)
     this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  providers(){
-    console.log(this.dataTable)
   }
 
 }
